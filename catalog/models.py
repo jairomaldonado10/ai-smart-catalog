e810df9638cc843d3cd9276687fbb45055766847
Jairo Maldonado <jairovillablanca8@gmail.com>
2026-04-23 22:54:46 -0400

    feat: add product model and run initial migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Nombre del Producto")
+    description = models.TextField(verbose_name="Descripción detallada")
+    price = models.IntegerField(verbose_name="Precio en CLP")
+    category = models.CharField(max_length=100, verbose_name="Categoría")
+    stock = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
